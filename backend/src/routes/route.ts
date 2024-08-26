f00cc2e19000895a1b88cefe19f66061cf67720a
Indrajit sahani <indrajit38mig@gmail.com>
2024-08-26 13:24:02 +0530

    added prisma migration initial

--- backend/src/routes/route.ts
+++ backend/src/routes/route.ts
@@ -1,10 +1,21 @@
 import { Hono } from 'hono'
-import { requestId } from 'hono/request-id'
 
-const router = new Hono()
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+
+
+const router = new Hono<{
+    Bindings: {
+		DATABASE_URL: string
+	}
+}>()
 
 router.get('/home', (c) => {
-  return c.text('Hello Hono!')
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+    return c.text('Hello Hono!')
 })
 
 router.post('/signin',(c)=>{
