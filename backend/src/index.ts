ff28f7e062f98a3df283ce7d0cb8cac75278bb33
Indrajit sahani <indrajit38mig@gmail.com>
2024-08-30 17:14:00 +0530

    cors config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,16 @@
 import { Hono } from 'hono'
 import router from './routes/route'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
+app.use(
+    '/api/v1/*',
+    cors({
+      origin: 'http://localhost:5173',
+      credentials: true,
+    })
+  )
+  
 
 app.route('/api/v1',router)
 
